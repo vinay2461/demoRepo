f38b155c85c52f7574fb4461d7c09451d139e7b7
vinay2461 <vinay.patil2461@gmail.com>
2021-07-27 19:41:25 +0530

    globantclass

---
+++ src/java_Practice/globantTest.java
@@ -0,0 +1,24 @@
+package java_Practice;
+
+public class globantTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="welcome globant";
+		
+		for(int i=0;i<=str.length()-1;i++) {
+			
+			//System.out.println(str.charAt(i));
+			
+			if(str.charAt(i)!=' ') {
+				for(int j=str.length()-1;j>=0;j--) {
+					
+					System.out.println(str.charAt(j));
+				}
+			}
+		}
+
+	}
+
+}
